778ff6d53f77593d747e06de8506d535cd957a80
singhalayush1998 <1998ayushsinghal@gmail.com>
2021-08-06 20:56:17 +0530

    public group schema and controler

---
+++ server/src/controllers/publicGroup.controller.js
@@ -0,0 +1,40 @@
+// const express = require('express');
+// const router = express.Router();
+// const Group = require("../modals/publicGroup.model");
+// const User = require("../modals/user.model")
+
+// router.post("/newGroup", async (req, res) => {
+//   try {
+//     const newGroup = await Group.create(req.body);
+//     res.status(201).json(newGroup);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/newGroup", async (req, res) => {
+//     try {
+//       const group = await Group.find();
+//       res.status(201).json(group);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+  
+// router.get("/newGroup/:id", async (req, res) => {
+//     try {
+//       const Individualgroup = await Group.findById(req.params.id);
+//       const {members} = Individualgroup 
+//       let membersArr = []
+
+//       for(let i = 0 ; i < members.length ; i++){
+//         const User = await User.findById(members[i])
+//         membersArr.push(User);
+//       }
+
+//       res.status(200).json({members:membersArr,Individualgroup});
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+// });
