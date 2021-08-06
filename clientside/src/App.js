96d158964a321f68ad9b8a1539c9abb925e4ad58
khushbookhator <khushbookhator11@gmail.com>
2021-08-06 19:25:48 +0530

    landing Page

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Landing } from './Components/Landing/Landing';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
+      <Landing/>
     </div>
   );
 }
