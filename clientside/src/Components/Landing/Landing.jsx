d051b303bb7c8cf9cb13e905c3fec399cb2a75f2
khushbookhator <khushbookhator11@gmail.com>
2021-08-07 00:46:55 +0530

    landing page

--- clientside/src/Components/Landing/Landing.jsx
+++ clientside/src/Components/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Intro } from './Details1'
 import { Details2 } from './Details2'
 
 const Landing = () => {
     return (
         <div>
+            <Intro/>
             <Details2/>
         </div>
     )
