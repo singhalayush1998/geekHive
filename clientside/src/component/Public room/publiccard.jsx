d547011fe00c62ac816891837097f64b27343582
unknown <aroul303@gmail.com>
2021-08-07 17:02:28 +0530

    leave and exit button

--- clientside/src/component/Public room/publiccard.jsx
+++ clientside/src/component/Public room/publiccard.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import "./publiccard.css"
-import {Link, Redirect} from "react-router-dom"
-import axios from 'axios'
-function Publiccard({group_name,description, roomid}) {
+import React from "react";
+import "./publiccard.css";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+function Publiccard({ group_name, description, roomid }) {
+  // const [added, setadded] = useState(false)
 
-    // const [added, setadded] = useState(false)
-
-    const goToRoom = (e) => {
-        if(!roomid){
-            e.preventDefault()
-        }
-        axios.patch(`http://localhost:1997/addmember?room=${roomid}`, {
-            id: "610e0b421369543e5c6abecf"
-        })
-        .then((res) => console.log(true))
+  const goToRoom = (e) => {
+    if (!roomid) {
+      e.preventDefault();
     }
+    axios
+      .patch(`http://localhost:1997/addmember?room=${roomid}`, {
+        id: "610e0b421369543e5c6abecf",
+      })
+      .then((res) => console.log(true));
+  };
 
-    // if(added){
-    //     return <Redirect push to=""/>
+  // if(added){
+  //     return <Redirect push to=""/>
 
-    // }
+  // }
 
-  let str=group_name.split("")
-  str=str[0]
+  let str = group_name.split("");
+  str = str[0];
 
-    return (
-        <>
-        <div className="card">
-            <div className="profile">
-               <h1>{str}</h1>
-            </div>
-          <div className="title" >
-            <h3>{group_name}</h3>
-            <p>{description}</p>
-            </div>
-        <Link onClick={(e) => goToRoom(e)} to={`/room/${roomid}`}> <div className="btn"> join</div></Link>
-            
+  return (
+    <>
+      <div className="card">
+        <div className="profile">
+          <h1>{str}</h1>
         </div>
-        </>
-    )
+        <div className="title">
+          <h3>{group_name}</h3>
+          <p>{description}</p>
+        </div>
+        <Link
+          style={{ textDecoration: "none" }}
+          onClick={(e) => goToRoom(e)}
+          to={`/room/${roomid}`}
+        >
+          {" "}
+          <div className="btn"> Join</div>
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default Publiccard
+export default Publiccard;
