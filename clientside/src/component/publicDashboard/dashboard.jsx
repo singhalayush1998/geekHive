69b0bf0f348acdad1482a39e917ee6b8a2b37767
unknown <aroul303@gmail.com>
2021-08-08 13:58:56 +0530

    profile page  design

--- clientside/src/component/publicDashboard/dashboard.jsx
+++ clientside/src/component/publicDashboard/dashboard.jsx
@@ -4,43 +4,38 @@ import logo from "../../assets/logo.jpeg";
 import Public from "../../assets/public.png";
 import Private from "../../assets/private.png";
 import mine from "../../assets/mygroups.png";
-import hive from "../../assets/hivetext.jpeg";
 import Publiccard from "../Public room/publiccard";
-import axios from "axios"
+import axios from "axios";
 import { Redirect, useHistory } from "react-router";
-import styled from "styled-components"
+import styled from "styled-components";
 import { Button, Modal, Paper, TextField } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import PrivateCard from "../privateDashboard/PrivateCard";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
 function Dashboard() {
-
   const classes = useStyles();
-  const [Publics, setpublic] = useState(true)
-  const [privates, setprivate] = useState(false)
-  const [privGroup, setPrivGroup] = useState([])
-  const [profile, setprofile] = useState(false)
-  const [groups, setGroups]= useState([])
-  // const [popup, setPopup] = useState(false)
-  const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
+  const [Publics, setpublic] = useState(true);
+  const [privates, setprivate] = useState(false);
+  const [privGroup, setPrivGroup] = useState([]);
+  const [profile, setprofile] = useState(false);
+  const [groups, setGroups] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
   const [open, setOpen] = React.useState(false);
-  const [query, setQuery] = useState("")
-  const [searchData, setSearchData] = useState([])
-  // const [restData, setRestData] = useState([])
-  // const [concats, setConcat] = 
+  const [query, setQuery] = useState("");
+  const [searchData, setSearchData] = useState([]);
+
 
   const handleOpen = () => {
     setOpen(true);
@@ -51,201 +46,212 @@ function Dashboard() {
   };
 
   const handleSearch = () => {
-    axios.get(`http://localhost:1997/search?s=${query}`)
-    .then((res) => searchParams(res.data.data))
-    .catch((err) => console.log(err))
-}
+    axios
+      .get(`http://localhost:1997/search?s=${query}`)
+      .then((res) => searchParams(res.data.data))
+      .catch((err) => console.log(err));
+  };
 
-  const searchParams= (data) => {
-    setSearchData(data)
-    let rest = []
+  const searchParams = (data) => {
+    setSearchData(data);
+    let rest = [];
 
-    for(let i=0; i<groups.length; i++){
-      for(let j=0; j<searchData.length; j++){
-        if(groups[i]._id !== searchData[j]._id){
-          rest.push(groups[i])
+    for (let i = 0; i < groups.length; i++) {
+      for (let j = 0; j < searchData.length; j++) {
+        if (groups[i]._id !== searchData[j]._id) {
+          rest.push(groups[i]);
         }
       }
     }
     // let rest = groups?.filter((i, index) => i.group_name !== searchData[index].group_name)
     // console.log(groups)
-    if(searchData){
-      setGroups([...searchData, ...rest])
+    if (searchData) {
+      setGroups([...searchData, ...rest]);
     }
-  }
+  };
 
   // useEffect(() => {
   //   searchParams()
   // },[groups])
 
-  const loginedUserId = JSON.parse(localStorage.getItem("user"))
-  const history = useHistory()
-
-
-  useEffect(() => {
-    axios.get("http://localhost:1997/newGroup")
-    .then((response) =>setGroups(response.data))
-
-  }, [open])
+  const loginedUserId = JSON.parse(localStorage.getItem("user"));
+  const history = useHistory();
 
   useEffect(() => {
-    axios.get("http://localhost:1997/newPrivateGroup")
-    .then((res) => setPrivGroup(res.data))
-  },[])
+    axios
+      .get("http://localhost:1997/newGroup")
+      .then((response) => setGroups(response.data));
+  }, [open]);
 
-  const handlepublic=()=>{ 
-    setpublic(true)
-    setprofile(false)
-    setprivate(false)
-  }
-  const handleprivate=()=>{ 
-    setpublic(false)
-    setprofile(false)
-    setprivate(true)
-    
-  }
-  const handleprofile=()=>{ 
-    setpublic(false)
-    setprofile(true)
-    setprivate(false)
-  }
-  const handleLogout = ()=>{
-    localStorage.clear()
-    history.push("/")
+  useEffect(() => {
+    axios
+      .get("http://localhost:1997/newPrivateGroup")
+      .then((res) => setPrivGroup(res.data));
+  }, []);
 
-  }
-    // loginedUserId = JSON.parse(loginedUserId)
-  useEffect(()=>{
-    if(!loginedUserId){
-      history.push("/login")
+  const handlepublic = () => {
+    setpublic(true);
+    setprofile(false);
+    setprivate(false);
+  };
+  const handleprivate = () => {
+    setpublic(false);
+    setprofile(false);
+    setprivate(true);
+  };
+  const handleprofile = () => {
+    setpublic(false);
+    setprofile(true);
+    setprivate(false);
+  };
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
+  // loginedUserId = JSON.parse(loginedUserId)
+  useEffect(() => {
+    if (!loginedUserId) {
+      history.push("/login");
     }
-  },[loginedUserId])
+  }, [loginedUserId]);
 
   const createGroup = () => {
     const payload = {
       group_name: title,
-      description: desc
-    }
+      description: desc,
+    };
 
-    axios.post("http://localhost:1997/newGroup", payload)
-    .then((res) => handleClose())
-    .catch((err) => console.log(err))
-    setTitle("")
-    setDesc("")
-  }
-
+    axios
+      .post("http://localhost:1997/newGroup", payload)
+      .then((res) => handleClose())
+      .catch((err) => console.log(err));
+    setTitle("");
+    setDesc("");
+  };
 
   return (
     <>
       <div className="dashboardBody">
-       
         <div className="container">
-
           <div className="profiles">
             {/* <img className="logo" src={logo} alt="" /> */}
             <div className="newone">
-                        {loginedUserId?.username[0]}
-                    </div>
+              {loginedUserId?.username[0].toUpperCase()}
+            </div>
             <div className="profiles-container" onClick={handlepublic}>
               <img src={Public} alt="" />
-              <strong>Public</strong>
-              <strong>Groups</strong>
-
+              <strong>Public Groups</strong>
+              <strong></strong>
             </div>
             <div className="profiles-container" onClick={handleprivate}>
               <img src={Private} alt="" />
-              <strong>Private</strong>
-              <strong>Groups</strong>
-
+              <strong>My Groups</strong>
+              {/* <strong>Groups</strong> */}
             </div>
             <div className="profiles-container" onClick={handleprofile}>
               <img src={mine} alt="" />
               <strong>Profile</strong>
             </div>
-          
           </div>
           <div className="right-container">
             <div className="nav-container">
               <img src={logo} alt="" />
               <input
-              className="searchbox"
+                className="searchbox"
                 placeholder="Find Rooms"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
-                />
-                <button className="searchbtn" onClick={handleSearch}> Search </button>
-              <button className="create" onClick={handleOpen}>Create New Group</button>
-              <div className="logout" onClick={handleLogout}>Logout</div>
+              />
+              <button className="searchbtn" onClick={handleSearch}>
+                {" "}
+                Search{" "}
+              </button>
+              <button className="create" onClick={handleOpen}>
+                Create New Group
+              </button>
+              <div className="logout" onClick={handleLogout}>
+                Logout
+              </div>
             </div>
             <div className="Select-container">
-
-
-              {Publics&& groups?.map(el =><Publiccard
-               group_name={el.group_name}
-               description={el.description}
-               members={el.members}
-               roomid={el._id}
-               
-
-              />) 
-
-              }
-              {privates&& privGroup?.map(el =><PrivateCard
-               group_name={el.group_name}
-               description={el.description}
-               passcode ={el.password}
-              //  members={el.members}
-               roomid={el._id}
-               
-
-              />) 
-
-              }
+              {Publics &&
+                groups?.map((el) => (
+                  <Publiccard
+                    group_name={el.group_name}
+                    description={el.description}
+                    members={el.members}
+                    roomid={el._id}
+                  />
+                ))}
+              {privates &&
+                privGroup?.map((el) => (
+                  <PrivateCard
+                    group_name={el.group_name}
+                    description={el.description}
+                    passcode={el.password}
+                    //  members={el.members}
+                    roomid={el._id}
+                  />
+                ))}
             </div>
-
-
-              
           </div>
           {
-            <Modal style={{
-              position: "fixed",
-              width: "40%",
-              height: "350px",
-              marginTop: "6%",
-              marginLeft:"30%",
-              // padding: "5%",
-            }} open={open} 
-            onClose={handleClose}>
-                <Paper style={{padding:"5%"}}>
-                  <h1>Create A New Group</h1>
-                  <TextField
-                    id="filled-basic"
-                    label="Group Name"
-                    variant="filled"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                  <br/><br/>
-                  <TextField
-                    id="filled-basic"
-                    label="Description"
-                    variant="filled"
-                    value={desc}
-                    onChange={(e) => setDesc(e.target.value)}
-                  />
-                  <br/><br/>
-                  <div style={{
+            <Modal
+              style={{
+                position: "fixed",
+                width: "40%",
+                height: "350px",
+                marginTop: "6%",
+                marginLeft: "30%",
+                // padding: "5%",
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <Paper style={{ padding: "5%" }}>
+                <h1>Create A New Group</h1>
+                <TextField
+                  id="filled-basic"
+                  label="Group Name"
+                  variant="filled"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <br />
+                <br />
+                <TextField
+                  id="filled-basic"
+                  label="Description"
+                  variant="filled"
+                  value={desc}
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <br />
+                <br />
+                <div
+                  style={{
                     display: "flex",
-                    justifyContent:"space-around"
-                  }}>
-                      <Button variant="contained" color="primary" onClick={handleClose}>Cancel</Button>
-                      <Button variant="contained" color="primary" onClick={createGroup}>Create</Button>
-                  </div>
-                </Paper>
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleClose}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={createGroup}
+                  >
+                    Create
+                  </Button>
+                </div>
+              </Paper>
             </Modal>
           }
-              
-
         </div>
       </div>
     </>
@@ -254,7 +260,4 @@ function Dashboard() {
 
 export default Dashboard;
 
-const Popup = styled.div`
-
-
-`
+const Popup = styled.div``;
